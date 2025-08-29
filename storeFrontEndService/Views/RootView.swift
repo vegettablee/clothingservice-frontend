e26d1112b14af65cd6568c4d273db1b9ff06f9e0
vegettablee <prestonrank5@gmail.com>
2025-08-29 15:34:53 -0500

    added more to the front-end

---
+++ storeFrontEndService/Views/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  storeFrontEndService
+//
+//  Created by Preston Rank on 8/27/25.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
---
+++ storeFrontEndService/Views/loadingScreen.swift
@@ -0,0 +1,8 @@
+//
+//  loadingScreen.swift
+//  storeFrontEndService
+//
+//  Created by Preston Rank on 8/27/25.
+//
+
+import Foundation
